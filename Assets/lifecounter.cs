aea2b895a08fa784cad22d030cf178d63f325127
ItsStanMarsh <35196896+ItsStanMarsh@users.noreply.github.com>
2018-04-19 09:54:13 +0200

    hi

---
+++ Assets/lifecounter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class lifecounter : MonoBehaviour
+{
+
+    public int lives = 20;
+    public Text uiText;
+
+    public Slider sl;
+
+    void Start()
+    {
+        sl = sl.GetComponent<Slider>();
+    }
+   
+    public void edditpoints(int hi)
+    {
+        if ((lives >= 20 && hi == 1) || (lives <= 0 && hi == -1))
+        {
+
+        }
+        else { lives += hi;}
+
+        sl.value = lives;
+    }
+
+
+     void Update()
+    {
+        uiText.text = "lives: " + lives.ToString();
+    }
+
+    
+}
---
+++ Assets/scorecounter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scorecounter : MonoBehaviour {
+
+    public int score = 0;
+    public Text uiText;
+
+	void Start () {
+		
+	}
+
+    public void hi(int tempA)
+    {
+        score += tempA;
+
+        UpdateText();
+    }
+
+
+    void UpdateText () {
+
+       uiText.text = "Score: " + score.ToString();
+    }
+}
